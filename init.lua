52df54c0dedcb2dee8738d04f578709305a3e081
noisycarlos <30759765+noisycarlos@users.noreply.github.com>
2023-09-23 07:37:16 -0700

    Updates init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,15 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+--Config files
+vim.api.nvim_set_keymap("n", "<leader>cc", ":e ~/AppData/Local/nvim/init.lua<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>cx", ":cd ~/AppData/Local/nvim<CR>", { noremap = true })
+--
+--Vim Config Shortcuts
+vim.api.nvim_set_keymap("n", "<leader>cc", ":e ~/AppData/Local/nvim/init.lua<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>cx", ":cd ~/AppData/Local/nvim<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>so", ":so %<CR>", { noremap = true })
+--
 -- Colemak nav
 vim.api.nvim_set_keymap("n", "m", "h", { noremap = true })
 vim.api.nvim_set_keymap("n", "n", "j", { noremap = true })
@@ -15,6 +24,7 @@ vim.api.nvim_set_keymap("n", "L", "E", { noremap = true })
 vim.api.nvim_set_keymap("n", "M", "H", { noremap = true })
 vim.api.nvim_set_keymap("n", "N", "J", { noremap = true })
 vim.api.nvim_set_keymap("n", "E", "i<CR><Esc>", { noremap = true })
+--
 -- vim.api.nvim_set_keymap('n', 'E', 'K', { noremap = true }) -- Uncomment if needed
 vim.api.nvim_set_keymap("n", "I", "L", { noremap = true })
 vim.api.nvim_set_keymap("n", "H", "I", { noremap = true })
@@ -40,3 +50,18 @@ vim.keymap.set("t", "<C-m>m", "<C-\\><C-n><C-w>h", { silent = true, noremap = tr
 vim.keymap.set("t", "<C-n>n", "<C-\\><C-n><C-w>j", { silent = true, noremap = true })
 vim.keymap.set("t", "<C-e>e", "<C-\\><C-n><C-w>k", { silent = true, noremap = true })
 vim.keymap.set("t", "<C-i>i", "<C-\\><C-n><C-w>l", { silent = true, noremap = true })
+
+-- Quality of life
+vim.api.nvim_set_keymap("n", "..", "o<Esc>k", { noremap = true })
+vim.api.nvim_set_keymap("n", ",,", "O<Esc>j", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>zz", { noremap = true })
+
+-- Frequent Projects
+vim.api.nvim_set_keymap("n", "<leader>1", ":cd ~/source/repos/Syncopotamus/Syncopotamus-go<CR>", { noremap = true })
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>2",
+  ":cd K:/Dropbox/Carlos/Documents/AutoHotkey/Function Keys<CR>",
+  { noremap = true }
+)
