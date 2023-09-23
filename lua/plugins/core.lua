a812446afc936c6e38e289c99e1a0c1bdba7814f
noisycarlos <30759765+noisycarlos@users.noreply.github.com>
2023-09-23 10:15:42 -0700

    Changes settings of nvim-notify

---
+++ lua/plugins/core.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "rcarriga/nvim-notify",
+    fps = 60,
+    top_down = true,
+    opts = {
+      -- level = 3,
+      render = "minimal",
+      -- stages = "static",
+    },
+  },
+}
