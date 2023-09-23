b4df120d084ec26cdee5aad9b701737995f14596
noisycarlos <30759765+noisycarlos@users.noreply.github.com>
2023-09-23 09:25:19 -0700

    Adds go

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,6 +10,7 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
@@ -33,6 +34,7 @@ require("lazy").setup({
       -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
+        -- "nvim-notify",
         -- "matchit",
         -- "matchparen",
         -- "netrwPlugin",
