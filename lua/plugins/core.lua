ddd7138eb4c1c21159f3d2cdeb0767dce4ad7b99
noisycarlos <30759765+noisycarlos@users.noreply.github.com>
2023-10-14 15:16:36 -0700

    Sets dot files visible by default

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -26,6 +26,22 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     keys = false,
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = false,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            ".git",
+            ".DS_Store",
+            "thumbs.db",
+          },
+          never_show = {},
+        },
+      },
+    },
   },
   {
     "ggandor/leap.nvim",
