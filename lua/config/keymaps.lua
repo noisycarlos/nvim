e6597394cf5d17587d66bdad5834e5f6fd5f51f3
Carlos Aldana <carlos@kp-r9.lan>
2023-11-16 09:23:08 -0800

    Reenables Jumplist binding

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -78,6 +78,8 @@ vim.keymap.set("t", "<C-i>i", "<C-\\><C-n><C-w>l", { silent = true, noremap = tr
 vim.api.nvim_set_keymap("n", "<C-,>", "<C-w>o", { noremap = true })
 
 --------- Convenience / Quality of Life
+vim.api.nvim_set_keymap("", "<C-h>", "<C-i>", { noremap = true }) -- Jumplist
+
 vim.api.nvim_set_keymap("n", "<leader>p", '"_diw"+P', { noremap = true }) -- Replace word under cursor
 vim.api.nvim_set_keymap("v", "<leader>p", '"_d"+P', { noremap = true }) -- Replace selection
 vim.api.nvim_set_keymap("n", "<C-S-x>", "10<C-x>", { noremap = true }) -- Increment number by 10
